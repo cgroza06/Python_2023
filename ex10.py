05187c460c72dd5d2c38fa39da4f4c1ad9a8b983
cgroza06 <100189134+cgroza06@users.noreply.github.com>
2023-10-11 18:27:00 +0300

    Delete ex10.py

--- ex10.py
+++
@@ -1,8 +0,0 @@
-def count_words(text):
-    words = text.split(" ")
-    return len(words)
-
-text = input("Introduceți un text: ")
-result = count_words(text)
-
-print(f"Numărul de cuvinte din text este: {result}")
