0872d6abcbcf4b610798e52764a8bfcfd3f24781
cgroza06 <100189134+cgroza06@users.noreply.github.com>
2023-10-11 18:27:59 +0300

    Delete ex8.py

--- ex8.py
+++
@@ -1,9 +0,0 @@
-def binary_number(x):
-    binary = bin(x)
-    no_of_one = binary.count("1")
-    return no_of_one
-
-n = int(input("Introduceti un numar: "))
-result = binary_number(n)
-
-print(f"Numărul de biți cu valoarea 1 în reprezentarea binară a numărului {n} este: {result}")
